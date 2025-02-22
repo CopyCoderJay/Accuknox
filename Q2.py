43af6b76b23ed5418eafe8757e6342812f1f6817
CopyCoderJay <sanjayoffcial07@gmail.com>
2025-02-22 21:47:49 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,3 +1,4 @@
+
 import threading
 from django.db import models
 from django.db.models.signals import post_save
@@ -28,3 +29,13 @@ def run_in_thread():
 
 # Run the test
 run_in_thread()
+
+"""
+Django signals run in the same thread as the caller. This is because Django signals are executed synchronously by default, meaning the signal handler runs in the same thread that triggered the signal.
+
+Explanation:
+Threading Setup: In this code, we use Python’s threading module to run the test_signal_thread function in a separate thread.
+Signal Handler: The print_thread function is connected to the post_save signal of the MyModel model. This function prints the name of the thread that executes the signal handler by calling threading.current_thread().name.
+Test Function: The test_signal_thread function creates a MyModel instance, which triggers the post_save signal. We then print the current thread name in both the signal handler and the test_signal_thread function.
+Thread Execution: The test is run in a new thread using the run_in_thread function, and we use thread.join() to wait for the thread to complete.
+"""
